c589953cd914fd0d708f04f12581143b413db7f6
Ilya Selenzev <blizard_sis@rambler.ru>
2024-08-22 14:20:27 +0300

    валидация отступов

--- src/scripts/validation.js
+++ src/scripts/validation.js
@@ -1,42 +1,42 @@
 // Функция для отображения ошибки
-function showInputError (formElement, inputElement, errorMessage, config) {
+function showInputError(formElement, inputElement, errorMessage, config) {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.add(config.inputErrorClass);
   errorElement.textContent = errorMessage;
   errorElement.classList.add(config.errorClass);
-};
+}
 
 // Функция для скрытия ошибки
-function hideInputError (formElement, inputElement, config) {
+function hideInputError(formElement, inputElement, config) {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.remove(config.inputErrorClass);
   errorElement.classList.remove(config.errorClass);
   errorElement.textContent = '';
-};
+}
 
 // Функция проверки валидации поля
-function checkInputValidity (formElement, inputElement, config) {
+function checkInputValidity(formElement, inputElement, config) {
   if (inputElement.validity.patternMismatch) {
     inputElement.setCustomValidity(inputElement.dataset.errorMessage);
   } else {
-    inputElement.setCustomValidity("");
+    inputElement.setCustomValidity('');
   }
   if (!inputElement.validity.valid) {
     showInputError(formElement, inputElement, inputElement.validationMessage, config);
   } else {
     hideInputError(formElement, inputElement, config);
   }
-};
+}
 
 // Функция для проверки наличия невалидных полей в форме
-function hasInvalidInput (inputList) {
+function hasInvalidInput(inputList) {
   return inputList.some((inputElement) => {
     return !inputElement.validity.valid;
   });
-};
+}
 
 // Функция переключения состояния кнопки отправки формы
-function toggleButtonState (inputList, buttonElement, config) {
+function toggleButtonState(inputList, buttonElement, config) {
   if (hasInvalidInput(inputList)) {
     buttonElement.disabled = true;
     buttonElement.classList.add(config.inactiveButtonClass);
@@ -44,10 +44,10 @@ function toggleButtonState (inputList, buttonElement, config) {
     buttonElement.disabled = false;
     buttonElement.classList.remove(config.inactiveButtonClass);
   }
-};
+}
 
 // Функция установки слушателей событий для полей формы
-function setEventListeners (formElement, config) {
+function setEventListeners(formElement, config) {
   const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
   const buttonElement = formElement.querySelector(config.submitButtonSelector);
 
@@ -59,10 +59,10 @@ function setEventListeners (formElement, config) {
       toggleButtonState(inputList, buttonElement, config);
     });
   });
-};
+}
 
 // Функция включения валидации для всех форм
-function enableValidation (config) {
+function enableValidation(config) {
   const formList = Array.from(document.querySelectorAll(config.formSelector));
   formList.forEach((formElement) => {
     formElement.addEventListener('submit', function (evt) {
@@ -70,10 +70,10 @@ function enableValidation (config) {
     });
     setEventListeners(formElement, config);
   });
-};
+}
 
 // Функция очистки ошибок валидации и деактивации кнопки
-function clearValidation (formElement, config) {
+function clearValidation(formElement, config) {
   const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
   const buttonElement = formElement.querySelector(config.submitButtonSelector);
 
@@ -83,6 +83,6 @@ function clearValidation (formElement, config) {
 
   buttonElement.disabled = true;
   buttonElement.classList.add(config.inactiveButtonClass);
-};
+}
 
 export { enableValidation, clearValidation };
